a59470dc92046c59692a40febe0dc7b4fb86362b
Misha7776 <vmisha7776@gmail.com>
2017-12-13 17:56:14 +0200

    Pattern for Heroes_app

---
+++ app-assets/js/scripts/ui/compact-menu.js
@@ -0,0 +1,14 @@
+/*=========================================================================================
+	File Name: compact-menu.js
+	Description: Compact menu page level only
+	----------------------------------------------------------------------------------------
+	Item Name: Stack - Responsive Admin Theme
+	Version: 2.0
+	Author: GeeksLabs
+	Author URL: http://www.themeforest.net/user/geekslabs
+==========================================================================================*/
+$(document).ready(function(){
+	setTimeout(function(){
+		$.app.menu.collapse();
+	}, 100);
+});
